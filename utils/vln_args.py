792e80794fd192c5037dd6939cae6d0f467ed3d2
tccoin <junzhewu@umich.edu>
2025-11-10 03:49:14 -0500

    fix args - episode_id->episode_label - test_id->test_scene_id - socket_server_host->host - socket_server_port->port

--- utils/vln_args.py
+++ utils/vln_args.py
@@ -1,12 +1,12 @@
 import os
 import sys
-import dotenv
+from dotenv import dotenv_values
 
 def add_vln_args(parser):
     arg_group = parser.add_argument_group("VLN benchmark", description="Arguments for VLN benchmark.")
     arg_group.add_argument("--episode_folder", type=str, default="episodes", help="Path to the episodes folder.")
-    arg_group.add_argument("--episode_id", type=str, default="test_generator_0", help="Test specific episode id.")
-    arg_group.add_argument("--test_id", type=str, default="test_generator", help="Scene id used for task generation.")
+    arg_group.add_argument("--episode_label", type=str, default="test_generator_0", help="Test specific episode label. Format: {scene_id}_{episode_id}), e.g., nv_apartment_0.")
+    arg_group.add_argument("--test_scene_id", type=str, default="test_generator", help="Scene id used for task generation.")
     arg_group.add_argument("--scene_folder", type=str, default=None, help="Path to the scene USD file.")
     arg_group.add_argument("--navmesh_path", type=str, default=None, help="Path to the navmesh file.")
     arg_group.add_argument("--disable_camera", type=bool, default=False, help="Disable camera.")
@@ -15,16 +15,16 @@ def add_vln_args(parser):
     arg_group.add_argument("--tg_config_path", type=str, default="episodes/task_config.yaml", help="Path to the task config file.")
     arg_group = parser.add_argument_group("Socket Server", description="Arguments for socket server.")
     arg_group.add_argument("--disable_socket_server", type=bool, default=False, help="Disable socket server.")
-    arg_group.add_argument("--socket_server_host", type=str, default="localhost", help="Host for socket server.")
-    arg_group.add_argument("--socket_server_port", type=int, default=12300, help="Port for socket server.")
+    arg_group.add_argument("--host", type=str, default="localhost", help="Host for socket server.")
+    arg_group.add_argument("--port", type=int, default=12300, help="Port for socket server.")
 
 def parse_args(parser):
     arg_list = sys.argv[1:]
     args = parser.parse_args()
-    dotenv.load_dotenv()
-    for key, value in os.environ.items():
-        if key.lower() in args.__dict__:
+    dot_args = dotenv_values(".env")
+    for key, value in dot_args.items():
+        if key.lower() in args.__dict__ and "--"+key.lower() not in arg_list:
             arg_list += [f"--{key.lower()}", value]
-            print(f"Using {key} from .env: {value}")
+            print(f"Using {key} from .env: {dot_args[key]}")
     args = parser.parse_args(arg_list)
     return args
