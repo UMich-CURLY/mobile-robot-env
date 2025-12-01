aded8b16db99f850745c431f42fc3badb9660d5c
tccoin <junzhewu@umich.edu>
2025-12-01 02:06:12 -0500

    update pyrecast lib version

--- tools/install_pyrecastdetour.sh
+++ tools/install_pyrecastdetour.sh
@@ -2,9 +2,9 @@
 # python3.8 -m pip uninstall scipy numpy matplotlib
 # python3.8 -m pip install scipy numpy matplotlib
 
-wget https://github.com/tccoin/PyRecastDetour-Sources/raw/refs/heads/main/dist/PyRecastDetour.cpython-310-x86_64-linux-gnu.so
+wget https://github.com/tccoin/PyRecastDetour-Sources/raw/refs/heads/main/dist/PyRecastDetour.cpython-311-x86_64-linux-gnu.so
 SITE_PACKAGES=$(python3 -c "import site; print(site.getsitepackages()[0])")
-sudo mv PyRecastDetour.cpython-310-x86_64-linux-gnu.so $SITE_PACKAGES/
+mv PyRecastDetour.cpython-310-x86_64-linux-gnu.so $SITE_PACKAGES/
 echo "Installed PyRecastDetour to $SITE_PACKAGES"
 echo "Testing import"
 python3 -c "import PyRecastDetour as m; print(m, 'OK')"
