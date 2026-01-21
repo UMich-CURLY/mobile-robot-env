4fcac31c9b91903b85b7628f47ecbcaa66725f64
tccoin <junzhewu@umich.edu>
2026-01-21 05:36:05 -0500

    fix navmesh settings

--- utils/navmesh_utils.py
+++ utils/navmesh_utils.py
@@ -56,7 +56,6 @@ def get_mesh(objs):
 
         if p.max()>1000 or p.min()<-1000:
             print(f"[WARNING]: Remote mesh found: {obj.GetPath()}, Max: {p.max()}, Min: {p.min()}")
-
         # print("points shape: ", p.shape)  n x 3
         # print("faces shape: ", f.shape)   n x 3
         points.extend(p)
